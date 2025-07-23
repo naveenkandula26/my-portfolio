9d623db94835592b78b140acd35ac28622efc181
naveenkandula26 <naveenkandula26@gmail.com>
2025-07-24 02:15:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../my-portfolio/App.tsx'
+import App from '../my-portfolio/App'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
