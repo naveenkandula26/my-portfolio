d03da7e87c41b0be089ee2d8571043c4ed87caee
naveenkandula26 <naveenkandula26@gmail.com>
2025-07-24 11:40:15 +0530

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,5 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
